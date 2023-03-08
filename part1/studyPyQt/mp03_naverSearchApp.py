5675374e22033bb20596e315c1d25a6e899f4caa
Gayeon_Lee913 <gy9411@naver.com>
2023-03-08 11:31:21 +0900

    Update mp03_naverSearchApp.py

--- part1/studyPyQt/mp03_naverSearchApp.py
+++ part1/studyPyQt/mp03_naverSearchApp.py
@@ -37,7 +37,6 @@ class qtApp(QWidget):
         else:
             api = NaverApi() # NaverApi 클래스 객체 생성
             node = 'news' # movie 로 변경하면 영화검색
-            outputs = [] # 결과 담을 리스트 변수
             display = 100
 
             result = api.get_naver_search(node, search, 1, display)
